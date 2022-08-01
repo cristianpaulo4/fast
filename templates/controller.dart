1a3fa792a891a1e803717be909e95c9ae06d034e
Cristian Paulo <cristianpaulo4@gmail.com>
2022-08-01 11:42:33 -0300

    Add progress

--- templates/controller.dart
+++ templates/controller.dart
@@ -2,9 +2,21 @@ String controllerTemplate = """
 import 'package:flutter/foundation.dart';
 
 class {{class-name}}Controller extends ChangeNotifier {
+  bool isProgress = false;
 
   void update() {
     notifyListeners();
   }  
+
+  void startProgress(){
+    isProgress = true;
+    update();  
+  }
+
+  void stopProgress(){
+    isProgress = false;
+    update();  
+  }
+  
 }
 """;
